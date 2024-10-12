930c94439113a655606781d8aa6478dcc73da5c3
njuiux-wp <njworkingprofessional9@gmail.com>
2024-10-13 02:00:19 +0530

    try to resolve live error draft 3

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; 
 import './App.css';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // Create a root
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
