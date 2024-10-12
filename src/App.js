f57a7790e6bc937c51e0d17e80ca10dde4143a52
njuiux-wp <njworkingprofessional9@gmail.com>
2024-10-13 01:20:42 +0530

    Updated: app.js for live error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Routes, Route, HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import BookingForm from './components/BookingForm';
 import Confirmation from './components/Confirmation';
