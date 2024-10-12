a87dbcb7afd1cbddfd25a4e8fb788c1ccdd1bff0
njuiux-wp <njworkingprofessional9@gmail.com>
2024-10-13 01:12:18 +0530

    Updated: app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter as Router } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import BookingForm from './components/BookingForm';
 import Confirmation from './components/Confirmation';
@@ -12,13 +12,15 @@ const App = () => {
   }, []);
 
   return (
-    <div className="wrapper-container w-full max-w-[400px] mx-auto min-h-screen px-[20px] py-[20px]">
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/booking" element={<BookingForm />} />
-        <Route path="/confirmation" element={<Confirmation />} />
-      </Routes>
-    </div>
+    <Router>
+      <div className="wrapper-container w-full max-w-[400px] mx-auto min-h-screen px-[20px] py-[20px]">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/booking" element={<BookingForm />} />
+          <Route path="/confirmation" element={<Confirmation />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
