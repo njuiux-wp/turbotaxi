708e4c5f1d27221bf877ad2610053bf22a37326b
njuiux-wp <njworkingprofessional9@gmail.com>
2024-10-13 21:03:12 +0530

    new design updates draft3

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import backgroundImage from '../assets/car-bg-new1.png';
+import backgroundImage from '../assets/car-icon.png';
 
 const Confirmation = () => {
   return (
     <div className="confirmation-wrapper">
       <h1 className="MainLogo text-center">Turbo<span>Taxi</span></h1>
-      <img src={backgroundImage} className="w-full" alt="image" />
+      <div className="car-animation-wrapper relative w-full h-[100px] flex items-center my-6">
+        <div className="car-animation">
+          <img src={backgroundImage} className="w-[150px]" alt="image" />
+        </div>
+      </div>
       <h2 className="text-lg text-white mb-10">Your booking has been confirmed!</h2>
       <Link to="/" className="primary-btn">Home</Link>
     </div>
